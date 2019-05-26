ed9bbd549d3c8715733e5294c15617088b53f2c0
wpdnakstp <wpdnakstp@likelion.org>
2019-05-26 16:44:11 +0900

    second

--- prpr/settings.py
+++ prpr/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tc5m43+$ecqz28e@1&#swubysrd9u9qm12#z802i_c@ytqge0-'
+SECRET_KEY =  os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+#  'tc5m43+$ecqz28e@1&#swubysrd9u9qm12#z802i_c@ytqge0-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
